3b454ba1a44881c25e4b1555b92af57c82144ef9
Erfan Poursina <erfanpoorsina@gmail.com>
2021-07-21 12:20:59 +0430

    Add wallet balances

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const TronWeb = require("tronweb");
 const TronGrid = require("trongrid");
 const walletAddressValidator = require("multicoin-address-validator").validate;
 const momentJalaali = require('moment-jalaali');
+const Big = require('big.js');
 momentJalaali.loadPersian({ usePersianDigits: true });
 
 const HttpProvider = TronWeb.providers.HttpProvider;
@@ -48,7 +49,7 @@ app.post('/getData', async (req, res) => {
     const transData = item.raw_data.contract[0].parameter.value;
 
     if(item.raw_data.contract[0].type === 'TransferContract') {
-      item['date'] = `${momentJalaali(item['block_timestamp']).format('jYYYY/jMM/jDD')} (${momentJalaali(item['block_timestamp']).fromNow()})`;
+      item['date'] = `${momentJalaali(item['block_timestamp']).format('HH:mm:ss - jYYYY/jMM/jDD')}`;
       item['from'] = TronWeb.address.fromHex(transData.owner_address);
       item['to'] = TronWeb.address.fromHex(transData.to_address);
   
@@ -57,11 +58,24 @@ app.post('/getData', async (req, res) => {
   });
 
   usdtTransactions.data.forEach(item => {
-    item['date'] = `${momentJalaali(item['block_timestamp']).format('jYYYY/jMM/jDD')} (${momentJalaali(item['block_timestamp']).fromNow()})`;
+    item['date'] = `${momentJalaali(item['block_timestamp']).format('HH:mm:ss - jYYYY/jMM/jDD')}`;
     convertedTransactions.push(item);
   });
 
-  return res.status(200).send(convertedTransactions);
+  // get trx balance
+  const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, '683D3E06C884D86407DC43049D78930E5FFA16AC017D2FFA2B8DC4DBF77A61BD');
+  const trxBalance = await tronWeb.trx.getBalance(walletAddress);
+
+  // get usdt balance
+  const contract = await tronWeb.contract().at('TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t');
+  const accountBalance = await contract.balanceOf(walletAddress).call();
+  const usdtBalance = new Big(accountBalance).toNumber() / 1000000;
+
+  return res.status(200).send({ 
+    trnasactions: convertedTransactions, 
+    trxBalance: trxBalance / 1000000,
+    usdtBalance
+  });
 });
 
 app.listen(process.env.PORT || 5000, () => {
